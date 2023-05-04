c728c89a37e412ca9fde81851cf5277ce673182d
Anton K <wiistriker@gmail.com>
2023-05-04 09:17:20 +0300

    update code

--- projects/ngx-splide/src/lib/ngx-splide.component.spec.ts
+++ projects/ngx-splide/src/lib/ngx-splide.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NgxSplideComponent } from './ngx-splide.component';
 
@@ -6,7 +6,7 @@ describe('NgxSplideComponent', () => {
   let component: NgxSplideComponent;
   let fixture: ComponentFixture<NgxSplideComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgxSplideComponent ]
     })
